86deb1df6462f89eba8107e2c2ed14b07b90a837
Xander Dumaine <xander.dumaine@gmail.com>
2013-03-29 10:02:02 -0400

    references

--- AvocadoHelper/AvocadoHelper/Models/AuthClient.cs
+++ AvocadoHelper/AvocadoHelper/Models/AuthClient.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using System.Collections.ObjectModel;
 
 namespace Avocado.Models
 {
